b780c52d7dc134bd4c75ad052c1c153d7b2025e9
Nick Turner <recbrasil5@gmail.com>
2026-03-28 13:04:28 -0500

    add server config to vite.config.ts to control port:3000.

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,22 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueDevTools from "vite-plugin-vue-devtools";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
   ],
   server: {
-    open: true,
+    port: 3000,
+    strictPort: true, // never increment, fail if 3000 is taken
+    open: false,      // do NOT auto-open browser
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-})
+});
